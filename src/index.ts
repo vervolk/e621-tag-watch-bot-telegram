e0dad728ed38abe15bdbb103448244ae232fc086
Lilith Tundrus <echofox123@gmail.com>
2018-03-14 19:26:18 -0400

    starting to use more config opts and the elapsedTime function from timer.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ import e621 from 'e621-api'
 import { e621PostData } from 'e621-api/build/interfaces';
 import { e621TagTypes, e621PopularityStrings } from 'e621-api/build/enums';
 import bot from './bot/bot-main';
-import { ver } from './config/config';
+import { ver, prod, debug } from './config/config';
+import { elapsedTime } from './lib/timer';
 
 // should probably put these in bot-main
 import rateLimit from 'telegraf-ratelimit';
@@ -18,8 +19,9 @@ import session from 'telegraf/session';
 
 
 // definitely going to need a DB for users from the other project
-
+if (debug) elapsedTime('Starting the logger...');
 let logger = new Logger('../logs', logLevels.error, true);
+if (debug) elapsedTime('Logger initialized');
 
 // Set limit to 1 message per 3 seconds using telegraf-ratelimit
 const limitConfig = {
