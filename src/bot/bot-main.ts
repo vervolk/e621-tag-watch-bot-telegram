d14738e7c3e7499d8149479440d29f6d6ae448ca
Lilith Tundrus <echofox123@gmail.com>
2018-03-13 21:38:46 -0400

    Should be the last time I need to change this.

--- src/bot/bot-main.ts
+++ src/bot/bot-main.ts
@@ -1,10 +1,7 @@
 'use strict';
-
 import Telegraf from 'telegraf';
-
 import { botToken } from '../config/config';
 
+// Create a new bot instance using our token and export it
 const bot = new Telegraf(botToken);
-
-
 export default bot;
