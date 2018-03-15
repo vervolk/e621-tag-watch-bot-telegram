dff61588400c16a31765cea11fa5a41322dc9f32
Lilith Tundrus <echofox123@gmail.com>
2018-03-14 21:39:54 -0400

    using context.time from elapsedTime

--- src/bot/commands/startHandler.ts
+++ src/bot/commands/startHandler.ts
@@ -1,5 +1,7 @@
 'use strict'
 
 export default async function startHandler(ctx) {
-    return ctx.reply(`Hello fren! I'm just a small bot, if you happen to find me, fow now I'm not yet working.`);
+    ctx.time('Sending welcome message...');
+    return ctx.reply(`Hello fren! I'm just a small bot, if you happen to find me, fow now I'm not yet working.`)
+    .then(() => ctx.time('Message sent!'))
 }
