a8d50e3400b8b09489ce115dbde454f23c03b255
Lilith Tundrus <echofox123@gmail.com>
2018-03-14 21:02:28 -0400

    building out bot structure

---
+++ src/bot/commands/index.ts
@@ -0,0 +1,11 @@
+'use strict';
+import Composer from 'telegraf';
+import startHandler from './startHandler';
+
+const composer = new Composer();
+
+
+composer.command('start', startHandler);
+
+
+export = composer;
