5bbed595c3594a4fd7e082a0f958c7e4905c6249
Lilith Tundrus <echofox123@gmail.com>
2018-03-15 20:09:22 -0400

    changed the start message

--- src/bot/commands/startHandler.ts
+++ src/bot/commands/startHandler.ts
@@ -4,6 +4,6 @@ export default async function startHandler(ctx) {
     ctx.resetTimer();
     ctx.time('Sending welcome message...');
     ctx.logger.info(`Start from ${JSON.stringify(ctx.message.from)}`)
-    return ctx.reply(`Hello fren! I'm just a small bot, if you happen to find me, fow now I'm not yet working.`)
+    return ctx.reply(`Hi! \n\nI'm a friendly bot that can help you keep track of some of your favorite tags as well as send you daily popular posts! **(Please note I'm still in development!!)**`)
         .then(() => ctx.time('Message sent!'));
 }
