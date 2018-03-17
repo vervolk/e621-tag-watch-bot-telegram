e9661bb33b05b794d1240f251670b1f5b156cab9
Lilith Tundrus <echofox123@gmail.com>
2018-03-17 02:41:09 -0400

    testing new API mthod from e621-api for Tags

--- src/bot/commands/tagWatchTest.ts
+++ src/bot/commands/tagWatchTest.ts
@@ -4,13 +4,16 @@ import { e621PostData } from 'e621-api/build/interfaces';
 import { e621TagTypes, e621PopularityStrings } from 'e621-api/build/enums';
 let wrapper = new e621('lilithtundrus/tag-watcher-test-0.0.1', 3);
 
+// We want this to take the user's tag request and keep a watch for that specific tag count to
+// change (up, not down due to deletes)
+
 export default async function tagWatchTest(ctx) {
     ctx.resetTimer();
     ctx.time('Running e621 tests...');
     ctx.logger.debug(`TEST from ${JSON.stringify(ctx.message.from.username)}`)
-    return wrapper.getE621PostIndexPaginate('werewolf order:favcount -flash -video', 0, 1, 3)
-        .then((e621PageSet) => {
-            return ctx.reply(e621PageSet[0][0].file_url)
+    return wrapper.getTagJSONByName('werewolf')
+        .then((data) => {
+            return ctx.reply(data)
         })
         .then(() => ctx.time('Message sent!'));
 }
