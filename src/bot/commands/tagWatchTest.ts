a0a6ba8385a80e6cc6aaa007f9724d53e15693e9
Lilith Tundrus <echofox123@gmail.com>
2018-03-18 16:50:01 -0400

    using ctx.wrapper now, also cleaned up the code a bit

--- src/bot/commands/tagWatchTest.ts
+++ src/bot/commands/tagWatchTest.ts
@@ -1,29 +1,27 @@
-'use strict'
-import e621 from 'e621-api';
-import { e621PostData } from 'e621-api/build/interfaces';
-import { e621TagTypes, e621PopularityStrings } from 'e621-api/build/enums';
-let wrapper = new e621('lilithtundrus/tag-watcher-test-0.0.1', 3);
-
+'use strict';
 // We want this to take the user's tag request and keep a watch for that specific tag count to
 // change (up, not down due to deletes)
 
 export default async function tagWatchTest(ctx) {
     ctx.resetTimer();
     ctx.time('Running e621 tests...');
-    ctx.logger.debug(`TEST from ${JSON.stringify(ctx.message.from.username)}`)
-    console.log(ctx.message.text.substring(6));
-    if (ctx.message.text.substring(6).length < 1) {
+    ctx.logger.debug(`TEST from ${JSON.stringify(ctx.message.from.username)}`);
+    // Get the tag to watch split away from the command
+    let tagString: string = ctx.message.text.substring(6);
+    if (tagString.length < 1) {
         return ctx.reply(`Please give a test tag to get the count of along with the /test command`)
             .then(() => ctx.time('Message sent!'));
     } else {
-        return wrapper.getTagJSONByName(ctx.message.text.substring(6))
+        // Attempt to get the tag data
+        return ctx.wrapper.getTagJSONByName(tagString)
             .then((data) => {
+                // an empty array is returned on no result
                 if (data.length < 1) {
-                    return ctx.reply(`Sorry, I couldn't get a count for the tag: ${ctx.message.text.substring(6)}`)
+                    return ctx.reply(`Sorry, I couldn't get a count for the tag: ${tagString}`)
+                        .then(() => ctx.time('Message sent!'));
                 }
-                return ctx.reply(`Count: ${data[0].count}`)
+                return ctx.reply(`Count: ${data[0].count}`);
             })
             .then(() => ctx.time('Message sent!'));
-    }   
-
+    }
 }
