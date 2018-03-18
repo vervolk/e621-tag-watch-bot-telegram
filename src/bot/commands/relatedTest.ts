ce59b71cf18c6affff42877154a742ea150c8640
Lilith Tundrus <echofox123@gmail.com>
2018-03-18 16:07:44 -0400

    running some more tag relation tests

--- src/bot/commands/relatedTest.ts
+++ src/bot/commands/relatedTest.ts
@@ -1,7 +1,7 @@
 'use strict'
 import e621 from 'e621-api';
 import { e621PostData } from 'e621-api/build/interfaces';
-import { e621TagTypes, e621PopularityStrings } from 'e621-api/build/enums';
+import { e621TagTypes, e621PopularityStrings, e621RelatedTagArrayTypes } from 'e621-api/build/enums';
 let wrapper = new e621('lilithtundrus/tag-watcher-test-0.0.1', 3);
 
 // We want this to take the user's tag request and keep a watch for that specific tag count to
@@ -18,17 +18,16 @@ export default async function relatedTagTest(ctx) {
     } else {
         return wrapper.getRelatedTagsByName(ctx.message.text.substring(9))
             .then((data) => {
-                console.log(Object.keys(data))
-                let key = Object.keys(data)[0];
-                console.log(data[key][0])
-                if (data[key][0] == undefined) {
-                    console.log(data)
+                console.log(data[0][e621RelatedTagArrayTypes.name])
+                if (data.length == 0) {
                     return ctx.reply(`Sorry, I couldn't get a set of related tags for the tag: ${ctx.message.text.substring(9)}`)
                 }
-                data[key].forEach(element => {
-                    console.log(element[0].name)
+                let messageArray = [];
+                data.forEach(element => {
+                    console.log(element[0])
+                    messageArray.push(element[0])
                 });
-                return ctx.reply(data)
+                return ctx.reply(messageArray.join('\n'))
             })
             .then(() => ctx.time('Message sent!'));
     }
