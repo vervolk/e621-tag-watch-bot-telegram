61bc71163f87c34c4e4b874e30748291d8d32a39
Lilith Tundrus <echofox123@gmail.com>
2018-03-19 18:39:18 -0400

    playing more with fiber, added some debugging info

--- src/lib/tagWatchClass.ts
+++ src/lib/tagWatchClass.ts
@@ -6,10 +6,12 @@ let Fiber = require('fibers');
 
 export default class TagWatcher {
     private teleCtx: any;
-    private originalCount: number
+    private originalCount: number;
+    private tag: string;
     constructor(teleCtx: any, originalCount: number) {
         this.teleCtx = teleCtx;
         this.originalCount = originalCount
+        this.tag = this.teleCtx.message.text.substring(7);
     }
     subscribe() {
         setInterval(this.fiberTest.bind(this), 15 * 1000)
@@ -40,35 +42,31 @@ export default class TagWatcher {
     }
 
     private fiberTest() {
-        console.log('Private method')
+        console.log(`Private function for ${this.tag}`);
         let test = Fiber(() => {
-            console.log('Fiber function')
-            console.log(this.originalCount)
-            this.teleCtx.wrapper.getTagJSONByName(this.teleCtx.message.text.substring(7))
+            console.log(`Fiber function for ${this.tag}`);
+            console.log(this.originalCount);
+            this.teleCtx.wrapper.getTagJSONByName(this.tag)
                 .then((data) => {
                     console.log(data[0])
                     if (data[0].count !== this.originalCount) {
                         // update the counter
                         this.originalCount = data[0].count
-                        return this.teleCtx.reply(`You've got new data aaaaa`)
+                        return this.teleCtx.reply(`You've got new data for ${this.tag}`)
                             .then(() => {
                                 // get the post (I think)
-                                this.teleCtx.wrapper.getE621PostIndexPaginate(this.teleCtx.message.text.substring(7), 0, 1, 1)
+                                this.teleCtx.wrapper.getE621PostIndexPaginate(this.tag, 0, 1, 1)
                                     .then((response) => {
                                         let reply = this.teleCtx.wrapper.generateE621PostUrl(response[0][0].id)
-                                        return this.teleCtx.reply(reply)
+                                        return this.teleCtx.reply(reply);
                                     })
                             })
-                .then(() => {
-                    Fiber.yield();
-                })
                     } else {
-                        this.teleCtx.logger.debug('No new data on the listener')
-                        Fiber.yield()
+                        this.teleCtx.logger.debug(`No new data for ${this.tag} listener`);
                     }
                 })
         })
-        test.run()
-        console.log('Back to private method')
+        test.run();
+        console.log('Back to private function');
     }
 }
