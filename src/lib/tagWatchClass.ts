711e2aeaa79b0b8af395fb9fffaab69e10d881ba
Lilith Tundrus <echofox123@gmail.com>
2018-03-19 18:39:57 -0400

    removed subscribeTest method

--- src/lib/tagWatchClass.ts
+++ src/lib/tagWatchClass.ts
@@ -17,30 +17,6 @@ export default class TagWatcher {
         setInterval(this.fiberTest.bind(this), 15 * 1000)
     }
 
-    private subscribeTest() {
-        // this is where we spawn a new process and set a check interval
-        console.log(this.originalCount)
-        this.teleCtx.wrapper.getTagJSONByName(this.teleCtx.message.text.substring(7))
-            .then((data) => {
-                console.log(data[0])
-                if (data[0].count !== this.originalCount) {
-                    // update the counter
-                    this.originalCount = data[0].count
-                    return this.teleCtx.reply(`You've got new data aaaaa`)
-                        .then(() => {
-                            // get the post (I think)
-                            this.teleCtx.wrapper.getE621PostIndexPaginate('fox', 0, 1, 1)
-                                .then((response) => {
-                                    let reply = this.teleCtx.wrapper.generateE621PostUrl(response[0][0].id)
-                                    return this.teleCtx.reply(reply)
-                                })
-                        })
-                } else {
-                    this.teleCtx.logger.debug('No new data on the listener')
-                }
-            })
-    }
-
     private fiberTest() {
         console.log(`Private function for ${this.tag}`);
         let test = Fiber(() => {
