56e10b05fca8ea3dd0c369c7b973be7bc0bd1449
Lilith Tundrus <echofox123@gmail.com>
2018-03-19 23:25:07 -0400

    filling out some of the functions

--- src/db/database.ts
+++ src/db/database.ts
@@ -23,9 +23,19 @@ export function createUserTable() {
         console.log('created main table');
     });
 }
-
-export function addUser() {
-
+/**
+ * Add a user to the DB, watchlist and blacklist are CSVs
+ * @export
+ * @param {number} teleid 
+ * @param {string} watchlist 
+ * @param {string} blackList 
+ */
+export function addUser(teleid: number, watchlist: string, blackList: string) {
+    var sql = `INSERT INTO userdata (teleid, watchlist, blacklist) VALUES ('${teleid}', '${watchlist}', '${blackList}')`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(`Created new user with ID: ${teleid} and a watchlist of ${watchlist}`);
+    });
 }
 
 export function modifyUser() {
@@ -38,4 +48,23 @@ export function removeUser() {
 
 export function getUserDataByID() {
 
+}
+/**
+ * 
+ * 
+ * @export
+ * @returns {Promise}
+ */
+export function getAllUserData() {
+    return new Promise((resolve, reject) => {
+        var sql = `SELECT * FROM userdata`;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            if (result.length < 1) {
+                return reject(`No user info found`);
+            } else {
+                return resolve(result);
+            }
+        });
+    });
 }
