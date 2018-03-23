849de379b2b1d457659895883f7ae8848c77c3e0
Lilith Tundrus <echofox123@gmail.com>
2018-03-22 21:26:24 -0400

    removed old commented code, going to add all of the features from it

--- src/bot/commands/watchHandler.ts
+++ src/bot/commands/watchHandler.ts
@@ -1,39 +1,6 @@
 'use strict';
 import TagWatchInitializer from '../../lib/newTagWatchClass';
 
-
-// export default async function watcHandler(ctx) {
-//     // TODO: Figure out how to use fiber to thread out the watch for each user!
-
-//     ctx.resetTimer();
-//     ctx.time('Running watch handler...');
-//     ctx.logger.debug(`/watch from ${JSON.stringify(ctx.message.from.username)}`)
-//     // Get the tag to search split away from the command
-//     let tagString: string = ctx.message.text.substring(7);
-//     if (tagString.length < 1) {
-//         return ctx.reply(`Please give a test tag to watch for you`)
-//             .then(() => ctx.time('Message sent!'));
-//     } else {
-//         // make sure the tag exists first
-//         // internally get the count
-//         // call the watcher class (I guess?)
-//         return ctx.wrapper.getTagJSONByName(tagString)
-//             .then((data) => {
-//                 // an empty array is returned on no result
-//                 if (data.length < 1) {
-//                     return ctx.reply(`Sorry, I couldn't find a tag named '${tagString}'. Make sure you aren't using ther shorthand for the tag!`)
-//                         .then(() => ctx.time('Message sent!'));
-//                 }
-//                 return ctx.reply(`Setting up hard coded event listener`)
-//                     .then(() => {
-//                         let tagWatcher = new TagWatcher(ctx, data[0].count);
-//                         return tagWatcher.subscribe();
-//                     })
-//             })
-//             .then(() => ctx.time('Message sent!'));
-//     }
-// }
-
 export default async function watcHandler(ctx) {
 
     // GET or CREATE the user in the DB and add the tag send through watch by the
